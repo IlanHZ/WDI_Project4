d53a4903c78a46bdf39158a7ef4c6779c934afdd
Ilan Hazan <ilaneliehazan@gmail.com>
2016-03-31 13:35:29 +0100

    files completed and everythings loaded

---
+++ api/config/app.js
@@ -0,0 +1,6 @@
+module.exports = {
+  port: process.env.PORT || 3000,
+  databaseUrl: 'mongodb://localhost/mappers',
+  secret: 'thisIsMappers',
+  appUrl: 'http://localhost:8000'
+}
---
+++ frontend/js/app.js
@@ -0,0 +1,20 @@
+angular
+  .module('mappersApp', ['satellizer', 'angular-jwt'])
+  .constant('API_URL', 'http://localhost:3000')
+  .config(oauthConfig);
+
+
+  oauthConfig.$inject = ['API_URL', '$authProvider', 'FACEBOOK_API_KEY'];
+  function oauthConfig(API_URL, $authProvider, FACEBOOK_API_KEY) {
+    // facebook = method given to us by satellizer
+    $authProvider.facebook({
+      // the place where we want fb to make its post request when we log in
+      url: API_URL + '/auth/facebook',
+      // our app id:
+      clientId: FACEBOOK_API_KEY,
+    })
+
+    
+    // store the token as "token", not satellizer_token, don't let people know which tech we re using
+    $authProvider.tokenPrefix = null;
+  }
