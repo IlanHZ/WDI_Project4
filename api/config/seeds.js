0fb804b4a8f93a35531eff123be70240ce3dd05b
Ilan Hazan <ilaneliehazan@gmail.com>
2016-04-01 15:38:49 +0100

    cleaned code and events working

--- api/config/seeds.js
+++ api/config/seeds.js
@@ -27,6 +27,27 @@ Event.create([{
   description: "description",
   lat: 18.312810846425442,
   lng: 103.974609375
+},
+{
+  title: "",
+  location: "Bruxelles",
+  type: "",
+  date: "",
+  time: 14.00,
+  description: "description",
+  lat: 50.54136296522161,
+  lng: 4.5263671875
+},
+{
+  title: "",
+  location: "Geneva",
+  type: "",
+  date: "",
+  time: 14.00,
+  description: "description",
+  lat: 46.316584181822186,
+  lng: 6.50390625
+
 }], function(err, events){
     if(err) console.error(err);
     else console.log(events);
--- frontend/js/directives/gmaps.js
+++ frontend/js/directives/gmaps.js
@@ -6,16 +6,18 @@ angular
 
 MapController.$inject = ["$resource"]
 function MapController($resource) {
-  var self = this;
-  var Event = $resource("http://localhost:3000/events/:id", { id: '@_id' }, { update: {method: 'PUT'}});
-  this.mapEventMarkers = Event.query(); 
-  console.log(this.mapEventMarkers)
 
+  var self = this;
+
+  // ng resource to access the back-end
+  var Event = $resource("http://localhost:3000/events/:id", { id: '@_id' }, { update: {method: 'PUT'}});
+
+  // get all the events 
+  this.mapEventMarkers = Event.query(); 
+  // console.log(this.mapEventMarkers)
+
+  // set the center of the map
   this.mapCenter = {lat: 13.736717, lng:  100.523186};
-  // this.mapMarkers = [{
-  //   name: "Testing Position",
-  //   position: { lat: 13.410994034321702, lng: 105.7763671875, }
-  // }]
 }
 
 
@@ -23,34 +25,31 @@ function MapController($resource) {
 function InitMap() {
 
   return {
-    // E(element <map></map>) | A(attribute map="something") | C(class class="map") | M(comment <!-- directive: map --> )
-    // Use mainly of the first two | use of element in this exemple
     restrict: 'E',
-    // Swap this tag with sth else (replace) | do I replace the directive with sth(TRUE) or replace what is inside  with sth(FALSE)
     replace: true,
-    // the actual code we will swap the directive for
     template: '<div class="google-map"></div>',
     scope: {
-      // '=' work as an ng-model, link controller-directive, expect an object from the controller| '@' except it to be a string (lat and lng in the map tag in html | '&' expect a function)
       center: '=',
       markers: '='
     },
-    // element is the DOM element as a jQuery object 
+
     link: function(scope, $element, attr) {
+
       if(!scope.center) throw new Error("You must provide a center for your map directive")
-      console.log("Scope center:", scope.center);
 
       var map = new google.maps.Map($element[0], {
         center: scope.center,
         zoom: 5,
       });
 
-      var allMarkers = [];
+      // array containing all the markers related to the events
+      var allEventsMarker = [];
       
       if(scope.markers) {
         
         scope.markers.$promise.then(function(markers) {
           console.log("Scope Markers:", markers);
+
           markers.forEach(function(marker) {
 
             var latLng = new google.maps.LatLng(marker.lat, marker.lng);
@@ -61,10 +60,11 @@ function InitMap() {
               animation: google.maps.Animation.DROP
             });
 
-            allMarkers.push(marker);
+            allEventsMarker.push(marker);
           });
         });
       }
+
       // create a polyline 
       poly = new google.maps.Polyline({
         strokeColor: '#000000',
@@ -72,24 +72,26 @@ function InitMap() {
         strokeWeight: 3
       });
       console.log("POLY:", poly);
+
       poly.setMap(map);
 
       // Add a listener for the click event
       map.addListener('click', addLatLng);
 
       // Handles click events on a map, and adds a new point to the Polyline.
-      function addLatLng(event) {
-        console.log(event.latLng)
+      function addLatLng(myTravelLine) {
+        console.log(myTravelLine.latLng)
+
         var path = poly.getPath();
         console.log("path:" + path)
 
         // Because path is an MVCArray, we can simply append a new coordinate
         // and it will automatically appear.
-        path.push(event.latLng);
+        path.push(myTravelLine.latLng);
 
         // Add a new marker at the new plotted point on the polyline.
         var marker = new google.maps.Marker({
-          position: event.latLng,
+          position: myTravelLine.latLng,
           title: '#' + path.getLength(),
           map: map
         });
@@ -102,14 +104,16 @@ function InitMap() {
           radius: 100000,    // 10 miles in metres
           fillColor: '#AA0000'
         });
+
         circle.bindTo('center', marker, 'position');
 
-        // var eventsPosition = new google.maps.latLng()
-        // check if a specific point is included in the radius of the polyline
         // isLocationOnEdge(point:LatLng, poly:Polygon|Polyline, tolerance?:number)
         console.log("marker position:", marker.position)
-        console.log(allMarkers);
-        allMarkers.forEach(function(marker) {
+        console.log(allEventsMarker);
+
+        allEventsMarker.forEach(function(marker) {
+
+          // isLocationOnEdge(point:LatLng, poly:Polygon|Polyline, tolerance?:number)
           if (google.maps.geometry.poly.isLocationOnEdge(marker.getPosition(), poly, 1)) {
             console.log("In the radius");
             marker.setMap(map);
@@ -125,7 +129,9 @@ function InitMap() {
       var infoWindow = new google.maps.InfoWindow({map: map});
 
       if (navigator.geolocation) {
+
         navigator.geolocation.getCurrentPosition(function(position) {
+
           var pos = {
             lat: position.coords.latitude,
             lng: position.coords.longitude
@@ -134,8 +140,11 @@ function InitMap() {
           console.log(position)
 
           infoWindow.setPosition(pos);
+
           infoWindow.setContent('Location found.');
+
           map.setCenter(pos);
+
         }, function() {
           handleLocationError(true, infoWindow, map.getCenter());
         });
@@ -144,7 +153,6 @@ function InitMap() {
         handleLocationError(false, infoWindow, map.getCenter());
       }
 
-
       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
           infoWindow.setPosition(pos);
           infoWindow.setContent(browserHasGeolocation ?'Error: The Geolocation service failed.': 'Error: Your browser doesn\'t support geolocation.');
@@ -152,29 +160,6 @@ function InitMap() {
     }
   }
 }
-
-
-
-
-  // function displayEvents() {
-  //   $.get('/events').then(function(data){
-
-  //     var events = data
-
-  //     events.forEach(function(event, idx){
-  //       setTimeout(function(){
-  //         var marker = new google.maps.Marker({
-
-  //           // parseFloat convert a string into a number
-  //           position: { lat: parseFloat(event.lat), lng: parseFloat(event.lng) },
-  //           map: map,
-  //           animation: google.maps.Animation.DROP,
-  //         });
-  //       });
-  //     });
-  //   });
-  // }
-
 
 
 
