6da68f0e29c3c2848e7a316f4439c8e23976d43a
Ilan Hazan <ilaneliehazan@gmail.com>
2016-04-17 17:31:42 +0100

    Updating env name for mongolab

--- config/app.js
+++ config/app.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.MONGOLAB_URI, 
+  databaseUrl: process.env.MONGODB_URI, 
   secret: 'thisIsMappers',
   appUrl: 'https://mappersapp.herokuapp.com/'
 }
